9d1de82ae8eabee435d8a1ab70d91982b43ab3ae
Kayden <27KlaRosa@live.ytech.edu>
2025-11-13 09:41:04 -0500

    sdsdsds

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ fbSocket.on("connect", () => {
 })
 
 // fbSocket.on("transferResponse", (response) => {
-    // console.log("Transfer Response:", response)
-    // response will be: { success: true/false, message: "..." }
+// console.log("Transfer Response:", response)
+// response will be: { success: true/false, message: "..." }
 // })
 
 //! End digipogs
@@ -241,8 +241,34 @@ io.on('connection', (socket) => {
         io.emit('updateBoard', data)
     })
 
+    //kaydens chat limiter
+    const chatLimit = {
+        count: 0,
+        lastReset: Date.now()
+    }
+    const CHAT_LIMIT = 5      // max messages
+    const CHAT_WINDOW = 10_000
+
     // When a message comes in
     socket.on('chatMessage', (msg) => {
+        const now = Date.now()
+
+        if (now - chatLimit.lastReset > CHAT_WINDOW) {
+            chatLimit.count = 0
+            chatLimit.lastReset = now
+        }
+
+        if (chatLimit.count >= CHAT_LIMIT) {
+            socket.emit('errorMessage', 'Youre sending messages too fast. Please wait a few seconds.')
+            return
+        }
+        chatLimit.count++
+
+        if (chatLimit.count >= CHAT_LIMIT) {
+            socket.emit('errorMessage', 'Muted for 10 seconds for spamming.')
+            socket.mutedUntil = Date.now() + 10_000
+            return
+        }
         if (user && user.game) user.game.chatMsg(user.displayName, msg)
     })
 
@@ -258,5 +284,6 @@ io.on('connection', (socket) => {
         }
     })
 })
+
 
 module.exports = { games }
