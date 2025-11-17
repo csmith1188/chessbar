044a9c125cfd34ccafc976cad73200672fea0fe6
Truit Elwell <145386841+pzahutdwg@users.noreply.github.com>
2025-11-17 09:02:20 -0500

    comments to INFOtemplate.js

--- INFOtemplate.js
+++ INFOtemplate.js
@@ -6,10 +6,12 @@ const EMPLOYEE_ID_2 = 2 // Second employee ID
 
 const POOL_ID = 0 // Pool ID
 
+//! For both FORMBAR_URL and THIS_URL:
+// Using `localhost:port` is generally a bad idea. Use your IP address instead, unless you are the only one testing.
 const FORMBAR_URL = 'https://formbar.url.com'
 const THIS_URL = '192.0.0.1'
 
-const FORMBAR_API_KEY = 'abcdef12356789'
+const FORMBAR_API_KEY = 'abcdef12356789' // Your API key (on your Formbar profile page)
 const FB_MIDDLEWARE_SECRET = 'secret_key!?!?!??'
 
 module.exports = { EMPLOYEE_ID_1, EMPLOYEE_ID_2, EMPLOYEE_PIN_1, EMPLOYEE_PIN_2, POOL_ID, FORMBAR_URL, THIS_URL, FORMBAR_API_KEY, FB_MIDDLEWARE_SECRET }
