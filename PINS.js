8789838b5c7b6a5067fea4c7d5d132bb4699acaa
Kayden <27KlaRosa@live.ytech.edu>
2025-11-17 13:36:29 -0500

    ??????????

---
+++ PINS.js
@@ -0,0 +1,7 @@
+const EMPLOYEE_PIN_1 = 1 // First employee pin
+const EMPLOYEE_PIN_2 = 200111 // Second employee pin
+
+const EMPLOYEE_ID_1 = 1 // First employee ID
+const EMPLOYEE_ID_2 = 40 // Second employee ID
+
+module.exports = { EMPLOYEE_ID_1, EMPLOYEE_ID_2, EMPLOYEE_PIN_1, EMPLOYEE_PIN_2 }
