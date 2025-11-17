272a916a6487897905f94e9a986312a3eaa23681
Truit Elwell <145386841+pzahutdwg@users.noreply.github.com>
2025-11-17 09:26:08 -0500

    Update INFOtemplate.js

--- INFOtemplate.js
+++ INFOtemplate.js
@@ -12,6 +12,6 @@ const FORMBAR_URL = 'https://formbar.url.com'
 const THIS_URL = '192.0.0.1'
 
 const FORMBAR_API_KEY = 'abcdef12356789' // Your API key (on your Formbar profile page)
-const FB_MIDDLEWARE_SECRET = 'secret_key!?!?!??'
+const FB_MIDDLEWARE_SECRET = 'secret_key!?!?!??' // Secret key for Formbar Oauth middleware
 
 module.exports = { EMPLOYEE_ID_1, EMPLOYEE_ID_2, EMPLOYEE_PIN_1, EMPLOYEE_PIN_2, POOL_ID, FORMBAR_URL, THIS_URL, FORMBAR_API_KEY, FB_MIDDLEWARE_SECRET }
