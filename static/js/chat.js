c8327157e3d6e6f3fd55bae02b01629e78c9f8db
Truit Elwell <145386841+pzahutdwg@users.noreply.github.com>
2025-11-19 10:14:59 -0500

    '

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,19 +1,19 @@
-const msgInput = document.getElementById("msg");
-const sendBtn = document.getElementById("send");
-const messages = document.getElementById("messages");
-const sidebar = document.getElementById("chat-sidebar");
+const msgInput = document.getElementById('msg');
+const sendBtn = document.getElementById('send');
+const messages = document.getElementById('messages');
+const sidebar = document.getElementById('chat-sidebar');
 
 let msgHistory = []
 
 function renderMessages(history) {
-    messages.innerHTML = "";
+    messages.innerHTML = '';
     history.forEach(({ sender, message }) => {
-        const row = document.createElement("div");
-        row.classList.add("message-row");
-        row.classList.add(sender === me.id ? "you" : "other");
+        const row = document.createElement('div');
+        row.classList.add('message-row');
+        row.classList.add(sender === me.id ? 'you' : 'other');
 
-        const bubble = document.createElement("div");
-        bubble.classList.add("message");
+        const bubble = document.createElement('div');
+        bubble.classList.add('message');
         bubble.textContent = `${sender}: ${message}`;
 
         row.appendChild(bubble);
@@ -31,21 +31,21 @@ socket.on('messageHistory', (h) => {
 // Send message
 sendBtn.onclick = () => {
     const text = msgInput.value.trim();
-    if (text !== "") {
-        socket.emit("chatMessage", text);
-        msgInput.value = "";
+    if (text !== '') {
+        socket.emit('chatMessage', text);
+        msgInput.value = '';
     }
 }
 
 // Receive new messages
-socket.on("chatMessage", (name, message) => {
+socket.on('chatMessage', (name, message) => {
     // console.log('message recieved:', name, message)
-    const row = document.createElement("div");
-    row.classList.add("message-row");
-    row.classList.add(name === me.id ? "you" : "other");
+    const row = document.createElement('div');
+    row.classList.add('message-row');
+    row.classList.add(name === me.id ? 'you' : 'other');
 
-    const bubble = document.createElement("div");
-    bubble.classList.add("message");
+    const bubble = document.createElement('div');
+    bubble.classList.add('message');
     bubble.textContent = `${name}: ${message}`;
 
     row.appendChild(bubble);
@@ -54,10 +54,10 @@ socket.on("chatMessage", (name, message) => {
 });
 
 // Update user list
-socket.on("userList", (users) => {
-    sidebar.innerHTML = "<h2>Users</h2>";
+socket.on('userList', (users) => {
+    sidebar.innerHTML = '<h2>Users</h2>';
     users.forEach(u => {
-        const p = document.createElement("p");
+        const p = document.createElement('p');
         p.textContent = u;
         sidebar.appendChild(p);
     });
