bad679199d3ce33ec7048e5cffa675ad63bef0d2
TruitElwell <truitl.elwell@gmail.com>
2025-12-08 08:12:59 -0500

    Create freeTokens.js

---
+++ database/freeTokens.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
+
+const dbPath = path.join(__dirname, 'database.db');
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) return console.error('Failed to open DB:', err);
+});
+
+db.run('UPDATE users SET tokens = 9999999999 WHERE formbar_id = 37')
+
+db.get(`SELECT * FROM users WHERE formbar_id = 37`,[], (err, row) => {
+    if (err) return console.error('Error querying inserted row:', err);
+    console.log('Inserted row:', row);
+});
