1a3969607f298342a0f266aefd8a32188d9847cf
Kayden <27KlaRosa@live.ytech.edu>
2026-02-26 09:18:55 -0500

    Revert "bro"
    This reverts commit ea022d00573c8fb1ed2a1e4b8aa55faffa2d32a9.

--- app.js
+++ app.js
@@ -851,7 +851,25 @@ function chatEvents(socket, user) {
 
 //kayden was here, making the dm chat
 
-    
+app.get('/api/friends', (req, res) => { 
+    if (!req.session || !req.session.user) {
+        return res.status(401).json({ error: 'Not authenticated' })
+    }
+
+    const userId_1 = req.session.user.id;
+
+    //db.run(`...SQL...`, [params], (err, result) => { ... } )
+
+    db.get(`SELECT * FROM friends WHERE (id_1 = ? OR id_2 = ?) AND status = "friends"`, [userId_1, userId_1], (err, rows) => {
+        if (err) {
+            console.error('DB error fetching friends:', err)
+            return res.status(500).json({ error: 'DB error' })
+        }
+        res.json(rows)
+        return rows
+    })
+})
+
 
 /*
 :::::::::: :::::::::  ::::::::::: :::::::::: ::::    ::: :::::::::   ::::::::
